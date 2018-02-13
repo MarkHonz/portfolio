35f0ee845bf07fb1ffb670504fb8e80789e99201
“MarkHonz” <m_honz@live.com>
2018-02-13 12:53:41 -0500

    first good

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables</title>
+</head>
+<body>
+    <?php // Script 2.3  - variables.php
+        
+        // An address:
+        $street = "100 Main St.";
+        $city = "State College";
+        $state = "PA";
+        $zip = 16801;
+
+        // Print the address:
+        echo "<p>The Address Is New :<br>$street<br>$city<br>$state<br>$zip</p>";
+    ?>    
+</body>
+</html>
